c7c68c6367e544ba67b6f0bab0f18788c1775482
mista <mr.bezdar@gmail.com>
2023-12-20 15:25:17 +0300

    1. Поле в админке по статусу заказа - в  списке заказов 2. После нажатия "клиент оплатил" проверка, что нет проданных товаров в заказе - подготовка 3. После нажатия "клиент оплатил" поменять sold у картин на True - подготовка

--- orders/admin.py
+++ orders/admin.py
@@ -3,40 +3,42 @@ from django.contrib import admin
 from django.contrib.admin.widgets import ForeignKeyRawIdWidget
 from .models import Order, OrderPainting
 
-class OrderPaintingForm(forms.ModelForm):
-    sold = forms.BooleanField(required=False)  # Добавляем поле для редактирования
 
-    class Meta:
-        model = OrderPainting
-        fields = '__all__'
+# class OrderPaintingForm(forms.ModelForm):
+#     class Meta:
+#         model = OrderPainting
+#         fields = '__all__'
 
-    def __init__(self, *args, **kwargs):
-        super(OrderPaintingForm, self).__init__(*args, **kwargs)
-        if self.instance and self.instance.painting_id:
-            self.fields['sold'].initial = self.instance.painting.sold
-
-    def save(self, commit=True):
-        instance = super(OrderPaintingForm, self).save(commit=False)
-        if instance.painting_id:
-            painting = instance.painting
-            painting.sold = self.cleaned_data['sold']
-            painting.save()
-        if commit:
-            instance.save()
-        return instance
 
 class OrderPaintingInline(admin.TabularInline):
     model = OrderPainting
     autocomplete_fields = ['painting']
-    form = OrderPaintingForm
-    fields = ['painting', 'sold', 'price']  # Добавляем поле sold для отображения
-    extra = 1
+    # form = OrderPaintingForm
+    fields = ('painting', 'display_sold', 'price')  # Добавляем поле sold для отображения
+    readonly_fields = ('display_sold',)
+    extra = 0
+
+    @admin.display(description="Наличие")
+    def display_sold(self, instance):
+        # Return the sold status from the related Painting instance
+        if not instance.painting:
+            return None
+        if instance.painting.sold:
+            return 'Продано'
+        else:
+            return 'Не продано'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city',
-                    'created', 'updated']
-    list_filter = ['created', 'updated']
+    list_display = ['id', 'full_name', 'status', 'email',
+                    'address', 'postal_code', 'city']
+    list_filter = ['created', 'status']
+    list_editable = ['status']
+    list_display_links = ['full_name']
     inlines = [OrderPaintingInline]
+    search_fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+
+    @admin.display(description="Наличие", ordering=('first_name'))
+    def full_name(self, order: Order):
+        return f'{order.first_name} {order.last_name}'
--- orders/models.py
+++ orders/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from shop.models import Painting
+from django.db import transaction
+from django.core.exceptions import ValidationError
 
 
 class Order(models.Model):
+    class Status(models.IntegerChoices):
+        NOT_PROCESSED = 0, 'Не обработан'
+        PROCESSED = 1, 'Обработан'
+        SUCCESS = 2, 'Завершен'
+        CANCELED = 3, 'Отменен'
+
     first_name = models.CharField(max_length=50, verbose_name='Имя')
     last_name = models.CharField(max_length=50, verbose_name='Фамилия')
     email = models.EmailField(verbose_name='Почта')
     address = models.CharField(max_length=250, verbose_name='Адрес')
     postal_code = models.CharField(max_length=20, verbose_name='Индекс')
     city = models.CharField(max_length=100, verbose_name='Город')
+    status = models.IntegerField(choices=tuple(map(lambda x: (x[0], x[1]), Status.choices)),
+                                 default=Status.NOT_PROCESSED, verbose_name="Статус")
     created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
     updated = models.DateTimeField(auto_now=True, verbose_name='Изменен')
 
@@ -26,11 +36,27 @@ class Order(models.Model):
     def get_total_cost(self):
         return sum(painting.get_cost() for painting in self.paintings.all())
 
+    def save(self, *args, **kwargs):
+        old_status = Order.objects.get(pk=self.pk).status
+        if self.status == self.Status.SUCCESS and old_status != self.Status.SUCCESS:
+            with transaction.atomic():
+                if any(op.painting.sold for op in self.order_paintings.all()):
+                    raise ValidationError("Один или несколько элементов в заказе уже проданы.")
+
+                for op in self.order_paintings.all():
+                    op.painting.sold = True
+                    op.painting.save()
+        super().save(*args, **kwargs)
+
 
 class OrderPainting(models.Model):
-    order = models.ForeignKey(Order, related_name='paintings', on_delete=models.CASCADE, verbose_name='Заказ')
-    painting = models.ForeignKey(Painting, related_name='order_paintings', on_delete=models.CASCADE, verbose_name='Картина')
+    order = models.ForeignKey(Order, related_name='order_paintings', on_delete=models.CASCADE, verbose_name='Заказ')
+    painting = models.ForeignKey(Painting, related_name='order_paintings', on_delete=models.CASCADE,
+                                 verbose_name='Картина')
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, verbose_name='Цена')
+    class Meta:
+        verbose_name = 'Картина в заказе'
+        verbose_name_plural = 'Картины в заказе'
 
     def __str__(self):
         return str(self.id)
