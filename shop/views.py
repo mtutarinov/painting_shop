b41ff1714fe6289eaf27df300fe6c42583247bce
mista <mr.bezdar@gmail.com>
2023-12-20 05:54:47 +0300

    menu deleted base.css and base.html corrections

--- shop/views.py
+++ shop/views.py
@@ -2,19 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Painting
 from cart.cart import Cart
 
-menu = [
-    # {'title': 'О художнике', 'url_name': ''},
-    {'title': 'Магазин', 'url_name': 'paintings'},
-    {'title': 'Контакты', 'url_name': 'contacts'}
-]
 
 
 def about(request):
-    return render(request, 'shop/painting/about.html', {'menu': menu})
+    return render(request, 'shop/painting/about.html')
 
 
 def contacts(request):
-    return render(request, 'shop/painting/contacts.html', {'menu': menu})
+    return render(request, 'shop/painting/contacts.html')
 
 
 def painting_list(request, category_slug=None):
@@ -27,14 +22,13 @@ def painting_list(request, category_slug=None):
     context = {
         'category': category,
         'categories': categories,
-        'paintings': paintings,
-        'menu': menu,
+        'paintings': paintings
     }
     return render(request, 'shop/painting/list.html', context=context)
 
 
 def painting_detail(request, id, slug):
     painting = get_object_or_404(Painting, id=id, slug=slug)
-    return render(request, 'shop/painting/detail.html', {'painting': painting, 'menu': menu})
+    return render(request, 'shop/painting/detail.html', {'painting': painting})
 
 
